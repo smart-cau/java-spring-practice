2c17a6139afa520e87efe3efadfb9da2bae45bba
HyeonWooJung <1995hyunwoo@gmail.com>
2021-07-16 17:33:01 +0900

    Spring Bean 등록 - Annotation 사용

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 // interface implements할 때 option + enter치면 쉽게 할 수 있음
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에서는, 아래 두 줄은 동시성 문제를 고려해야 함
